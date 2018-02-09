5e6ce1940cd7d43555ad931e4045d39c95821b96
Vladimir Sipko <v.sipko@allmax.team>
2018-02-09 09:30:47 +0700

    fixed: Excessive checks

--- contracts/BurnableToken.sol
+++ contracts/BurnableToken.sol
@@ -76,7 +76,6 @@ contract BurnableToken is StandardToken, AddressesBurn {
   function burn(uint256 _value) public {
     require(isAllowed(msg.sender));
     require(_value > 0);
-    require(_value <= balances[msg.sender]);
 
     // no need to require value <= totalSupply, since that would imply the
     // sender's balance is greater than the totalSupply, which *should* be an assertion failure
